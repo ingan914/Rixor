fa70a8267a09a18cc1cf79ee03ebdb9507f646b4
bookofnathan <bookofnathan@gmail.com>
2014-03-12 17:44:07 -0500

    Rework Contributor to handle people with no contribution

--- src/main/java/com/projectrixor/rixor/scrimmage/map/extras/Contributor.java
+++ src/main/java/com/projectrixor/rixor/scrimmage/map/extras/Contributor.java
@@ -1,15 +1,39 @@
 package com.projectrixor.rixor.scrimmage.map.extras;
 
-import lombok.Getter;
+public class Contributor
+{
+    protected final String name;
+    protected final String contribution;
 
-public class Contributor {
-	
-	@Getter String name;
-	@Getter String contribution;
-	
-	public Contributor(String name, String contribution) {
-		this.name = name;
-		this.contribution = contribution;
-	}
-	
+    public Contributor(String name)
+    {
+        this.name = name;
+        this.contribution = null;
+    }
+
+    public Contributor(String name, String contribution)
+    {
+        this.name = name;
+        this.contribution = contribution;
+    }
+
+    public String toString()
+    {
+        return getName();
+    }
+
+    public String getName()
+    {
+        return this.name;
+    }
+
+    public boolean hasContribution()
+    {
+        return this.contribution != null;
+    }
+
+    public String getContribution()
+    {
+        return this.contribution;
+    }
 }
